5e40e936482ddbbe7c7e1c6db740a9be236e4081
LarsPeter <larspeter94@gmail.com>
2017-09-26 15:48:36 +0200

    ry

--- BachelorModelViewController/Controllers/HomeController.cs
+++ BachelorModelViewController/Controllers/HomeController.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace BachelorModelViewController.Controllers
 {
     public class HomeController : Controller
     {
+        static HttpClient client = new HttpClient();
+        
+        static async Task<string> GetStringValue()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                return await httpClient.GetStringAsync("http://localhost:12345/api/values/5");
+            }
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -22,7 +34,7 @@ namespace BachelorModelViewController.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = GetStringValue().Result;
 
             return View();
         }
