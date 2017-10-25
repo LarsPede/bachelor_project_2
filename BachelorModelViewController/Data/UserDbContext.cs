1a1bf180034c13467330695ffc4044f120b1a2cf
LarsPeter <larspeter94@gmail.com>
2017-10-25 13:52:00 +0200

    forgot newest version

--- BachelorModelViewController/Data/UserDbContext.cs
+++ BachelorModelViewController/Data/UserDbContext.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BachelorModelViewController.Models;
 
 namespace BachelorModelViewController.Data
 {
-    public class UserDbContext
+    public class UserDbContext : IdentityDbContext<User>
     {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
