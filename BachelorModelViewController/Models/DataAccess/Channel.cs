6823143520284297f2c339b881323341750aba59
Gilsmeister <mortendamgilsaa@gmail.com>
2017-10-25 13:16:03 +0200

    more changes

---
+++ BachelorModelViewController/Models/DataAccess/Channel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BachelorModelViewController.Models.DataAccess
+{
+    public class Channel : DbContext
+    {
+        public List<Guid> AccessIn { get; set; }
+        public List<Guid> AccessOut { get; set; }
+        public string EntryPoint { get; set; } // path in
+        public string EndPoint { get; set; } // path out
+
+    }
+}
---
+++ BachelorModelViewController/Models/DataAccess/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BachelorModelViewController.Models.DataAccess
+{
+    class User : DbContext
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        
+
+    }
+}
