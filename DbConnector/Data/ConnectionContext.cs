73418d6db3a980ea51f88dfc4d86cbb0c3bde5d4
Morten Dam Gilså <mortendamgilsaa@gmail.com>
2017-10-25 11:39:08 +0200

    latest

---
+++ DbConnector/Data/ConnectionContext.cs
@@ -0,0 +1,28 @@
+﻿using DbConnector.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DbConnector.Data
+{
+    public class ConnectionContext : DbContext
+    {
+        public ConnectionContext(DbContextOptions<ConnectionContext> options) : base(options)
+        {
+        }
+
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Users_UserGroups> Users_UserGroups{ get; set; }
+        public DbSet<UserGroup> UserGroups { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("User");
+            modelBuilder.Entity<Users_UserGroups>().ToTable("Users_UserGroups");
+            modelBuilder.Entity<UserGroup>().ToTable("UserGroup");
+        }
+    }
+}
---
+++ DbConnector/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace DbConnector
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<DbConnector.Data.ConnectionContext>();
+                }
+                catch(Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError("An error occurred while seeding the database." + ex);
+                }
+            }
+
+            host.Run();
+        }
+    }
+}
