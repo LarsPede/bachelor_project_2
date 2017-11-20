dd9899886c1f788dfecc3adc3b47aae10903b06b
Gilsmeister <mortendamgilsaa@gmail.com>
2017-11-20 20:04:52 +0100

    something

---
+++ BachelorModelViewController/Models/ViewModels/AccessViewModel/AccessCredentialViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BachelorModelViewController.Models.ViewModels.AccessViewModel
+{
+    public class AccessCredentialViewModel
+    {
+        public Guid Id { get; set; }
+        public Guid RoleId { get; set; }
+        [ForeignKey("RoleId")]
+        public IdentityRole Role { get; set; }
+        public bool groupDemanded { get; set; }
+        public bool userDemanded { get; set; }
+
+    }
+}
